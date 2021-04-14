f00ff27b3095084bc14f59c74bfde9d890ba7dbb
Irreprochable <80385586+Irreprochable@users.noreply.github.com>
2021-04-14 09:59:55 +0200

    On enlève le superflu

--- le_projecto.py
+++ le_projecto.py
@@ -16,74 +16,88 @@ pygame.display.set_icon(image)
 #Titre
 font = pygame.font.SysFont("Carlito Bold", 54)
 img_txt = font.render("Paint 3.0",True,black)
-pygame.Surface.blit(window,img_txt,(30,600))
+pygame.Surface.blit(window,img_txt,(35,600))
 
 
-le_start = pygame.Rect(20,20,150,75)
-pygame.draw.rect(window,black,le_start)
-start = pygame.image.load('Staart.png').convert()
-pygame.Surface.blit(window, start, (20,20))
+boutton_start = pygame.Rect(20,20,150,75)
+pygame.draw.rect(window,black,boutton_start)
+logo_start = pygame.image.load('Start.png').convert()
+pygame.Surface.blit(window, logo_start, (20,20))
 
 
-le_stop = pygame.Rect(20,115,150,75)
-pygame.draw.rect(window,black,le_stop)
-stop = pygame.image.load('Stoop.png').convert()
-pygame.Surface.blit(window, stop, (20,115))
+boutton_stop = pygame.Rect(20,115,150,75)
+pygame.draw.rect(window,black,boutton_stop)
+logo_stop = pygame.image.load('Stop.png').convert()
+pygame.Surface.blit(window, logo_stop, (20,115))
 
-le_triangle = pygame.Rect(392,500,50,50)
-pygame.draw.rect(window,black,le_triangle)
-triangle = pygame.image.load('Triangle.png').convert()
-pygame.Surface.blit(window, triangle, (392,500))
+boutton_triangle = pygame.Rect(392,520,50,50)
+pygame.draw.rect(window,black,boutton_triangle)
+logo_triangle = pygame.image.load('Triangle.png').convert()
+pygame.Surface.blit(window, logo_triangle, (392,520))
 
-le_carre = pygame.Rect(592,500,50,50)
-pygame.draw.rect(window,black,le_carre)
-carre = pygame.image.load('Carré.png').convert()
-pygame.Surface.blit(window, carre, (592,500))
+boutton_carre = pygame.Rect(592,520,50,50)
+pygame.draw.rect(window,black,boutton_carre)
+logo_carre = pygame.image.load('Carré.png').convert()
+pygame.Surface.blit(window, logo_carre, (592,520))
 
-le_rond = pygame.Rect(792,500,50,50)
-pygame.draw.rect(window,black,le_rond)
-rond = pygame.image.load('Rond.png').convert()
-pygame.Surface.blit(window, rond, (792,500))
+boutton_rond = pygame.Rect(792,520,50,50)
+pygame.draw.rect(window,black,boutton_rond)
+logo_rond = pygame.image.load('Rond.png').convert()
+pygame.Surface.blit(window, logo_rond, (792,520))
 
 
+lerectancle = pygame.image.load('rectancle.png').convert()
+pygame.Surface.blit(window,lerectancle,(272,20))
 
-lerectancle = pygame.Rect(190, 100, 200, 200)
-pygame.draw.rect(window, black,lerectancle )
+boutton_color = pygame.Rect(30,400,178,178)
+pygame.draw.rect(window,gray,boutton_color)
+color = pygame.image.load('COLOR.png').convert_alpha()
 
-print("test")
+dessin_carre = pygame.Surface(50,50)
+
 running = True
 while running:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
-
 
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             coordinates = pygame.mouse.get_pos()
 
-                # mouse_position is in the form [x,y], we only want the x part
+# mouse_position is in the form [x,y], we only want the x part
             zx = coordinates[0]
             zy = coordinates[1]
 
-                #test si la zone de clique correspond à la bonne reponse
-            if lerectancle.collidepoint(zx, zy):
-               print("ok")
+#test si la zone de clique correspond à la bonne réponse
 
-            if le_start.collidepoint(zx, zy):
+            if boutton_start.collidepoint(zx, zy):
                print("start")
 
-            if le_stop.collidepoint(zx, zy):
+            if boutton_stop.collidepoint(zx, zy):
                print("stop")
 
-            if le_triangle.collidepoint(zx, zy):
-               print("triangle")
+            if boutton_triangle.collidepoint(zx, zy):
 
-            if le_carre.collidepoint(zx, zy):
-               print("carre")
+                pygame.Surface.blit(window, color, (30, 400))
 
-            if le_rond.collidepoint(zx, zy):
-               print("rond")
+
+
+            if boutton_carre.collidepoint(zx, zy):
+
+                pygame.Surface.blit(window, color, (30, 400))
+                pygame.draw.rect(lerectancle,black,dessin_carre,(100,100))
+
+
+            if boutton_rond.collidepoint(zx, zy):
+
+                pygame.Surface.blit(window, color, (30, 400))
+
+
+            if boutton_color.collidepoint(zx, zy):
+
+                pygame.draw.rect(window,gray,boutton_color)
 
 
     pygame.display.flip()
